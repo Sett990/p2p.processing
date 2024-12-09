a2ebd388fa021c08681773d1f9a4197d7f9f6369
Niiikkid <niiikkid@gmail.com>
2024-12-09 20:43:51 +1000

    ui

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -22,17 +22,17 @@ defineOptions({ layout: AuthenticatedLayout })
 
     <div>
         <div>
-            <div class="max-w-7xl mx-auto space-y-6">
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-xl ">
+            <div class="max-w-7xl grid grid-cols-1 xl:grid-cols-2 gap-6">
+                <div class="p-5 sm:p-8 bg-white dark:bg-gray-800 shadow rounded-plate lg:w-fit w-full">
                     <UpdateProfileInformationForm
                         :must-verify-email="mustVerifyEmail"
                         :status="status"
-                        class="max-w-xl"
+                        class="w-full lg:max-w-lg"
                     />
                 </div>
 
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-xl ">
-                    <UpdatePasswordForm class="max-w-xl" />
+                <div class="p-5 sm:p-8 bg-white dark:bg-gray-800 shadow rounded-plate lg:w-fit w-full">
+                    <UpdatePasswordForm class="w-full lg:max-w-lg" />
                 </div>
 <!--
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-xl ">
