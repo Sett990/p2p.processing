b1c53f9e7bd264d6079583a36648467a273f2489
Niiikkid <niiikkid@gmail.com>
2024-12-12 01:11:39 +1000

    fix

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -16,7 +16,7 @@ const userStore = useUserStore();
 const rates = ref(
     usePage().props.data.rates.sort((item) => {
         return ['rub', 'usd', 'eur'].includes(item.code)
-    })
+    }).reverse()
 );
 
 const showAllRates = ref(false);
@@ -54,10 +54,9 @@ onMounted(() => {
 
 router.on('success', (event) => {
     initFlowbite();
-
     rates.value = usePage().props.data.rates.sort((item) => {
         return ['rub', 'usd', 'eur'].includes(item.code)
-    });
+    }).reverse();
 })
 
 const openDocs = () => {
