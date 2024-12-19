4f2356c125c740f2502eab28188b6226269031d2
Niiikkid <niiikkid@gmail.com>
2024-12-20 09:45:28 +1000

    payout offer

--- app/Enums/PayoutSubStatus.php
+++ app/Enums/PayoutSubStatus.php
@@ -7,4 +7,9 @@ use App\Traits\Enumable;
 enum PayoutSubStatus: string
 {
     use Enumable;
+
+    case DISTRIBUTION = 'distribution';
+    case PROCESSING_BY_TRADER = 'processing_by_trader';
+    case PROCESSING_BY_ADMINISTRATOR = 'processing_by_administrator';
+    case FULLY_COMPLETED = 'fully_completed';
 }
