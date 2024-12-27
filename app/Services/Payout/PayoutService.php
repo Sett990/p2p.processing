558262536397e1ff11293e8b18e68ebf5041c7e2
Niiikkid <niiikkid@gmail.com>
2024-12-27 13:11:56 +1000

    payout menu

--- app/Services/Payout/PayoutService.php
+++ app/Services/Payout/PayoutService.php
@@ -13,6 +13,7 @@ use App\Models\Payout;
 use App\Models\PayoutOffer;
 use App\Services\Money\Currency;
 use App\Services\Money\Money;
+use App\Services\Payout\Utils\OffersMenu;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
@@ -79,76 +80,12 @@ class PayoutService implements PayoutServiceContract
 
     public function getOffersMenu(): array
     {
-        $groupedPayoutOffers = PayoutOffer::query()
-            ->with('paymentGateway', 'owner')
-            ->where('active', true)
-            ->get()
-            ->mapToGroups(function (PayoutOffer $payoutOffer) {
-                return [$payoutOffer->paymentGateway->code => $payoutOffer];
-            });
-
-        $aggregatedOffers = [];
-
-        foreach ($groupedPayoutOffers as $key => $payoutOffers) {
-            foreach ($payoutOffers as $payoutOffer) {
-                /**
-                 * @var PayoutOffer $payoutOffer
-                 */
-                if (empty($aggregatedOffers[$key])) {
-                    $aggregatedOffers[$key] = [
-                        'max_amount' => $payoutOffer->max_amount,
-                        'min_amount' => $payoutOffer->min_amount,
-                        'currency' => $payoutOffer->currency->getCode(),
-                        'detail_type' => $payoutOffer->detail_types->first()->value,
-                        'payment_gateway' => [
-                            'name' => $payoutOffer->paymentGateway->name,
-                            'name_with_currency' => $payoutOffer->paymentGateway->name_with_currency,
-                            'code' => $payoutOffer->paymentGateway->code,
-                        ],
-                        'offers_count' => 1
-                    ];
-                }
-
-                if ($aggregatedOffers[$key]['max_amount']->lessThan($payoutOffer->max_amount)) {
-                    $aggregatedOffers[$key]['max_amount'] = $payoutOffer->max_amount;
-                }
-                if ($aggregatedOffers[$key]['min_amount']->lessThan($payoutOffer->min_amount)) {
-                    $aggregatedOffers[$key]['min_amount'] = $payoutOffer->min_amount;
-                }
-
-                $aggregatedOffers[$key]['offers_count'] += 1;
-            }
-        }
-
-        foreach ($groupedPayoutOffers as $key => $payoutOffers) {
-            foreach ($payoutOffers as $payoutOffer) {
-                $aggregatedOffers[$key]['recommended_max_amount'][] = intval($payoutOffer->max_amount->toBeauty());
-                $aggregatedOffers[$key]['recommended_min_amount'][] = intval($payoutOffer->min_amount->toBeauty());
-            }
-            if (count($aggregatedOffers[$key]['recommended_max_amount']) <= 2) {
-                $aggregatedOffers[$key]['recommended_max_amount'] = $aggregatedOffers[$key]['recommended_max_amount'][0];
-                $aggregatedOffers[$key]['recommended_min_amount'] = $aggregatedOffers[$key]['recommended_min_amount'][0];
-                continue;
-            }
-            sort($aggregatedOffers[$key]['recommended_max_amount']);
-            sort($aggregatedOffers[$key]['recommended_min_amount']);
-
-            $aggregatedOffers[$key]['recommended_max_amount'] = $aggregatedOffers[$key]['recommended_max_amount'][intval(count($aggregatedOffers[$key]['recommended_max_amount']) / 2) + 1];
-            $aggregatedOffers[$key]['recommended_min_amount'] = $aggregatedOffers[$key]['recommended_min_amount'][intval(count($aggregatedOffers[$key]['recommended_min_amount']) / 2) + 1];
-        }
-
-
-        foreach ($aggregatedOffers as $key => $offer) {
-            $aggregatedOffers[$key]['max_amount'] = $offer['max_amount']->toBeauty();
-            $aggregatedOffers[$key]['min_amount'] = $offer['min_amount']->toBeauty();
-        }
-
-        return $aggregatedOffers;
+        return OffersMenu::getMenu();
     }
 
     public function makeOffersMenu(): void
     {
-
+        OffersMenu::updateMenu();
     }
 
     private function getPayoutOffer(Money $amount, DetailType $detailType, PaymentGateway $paymentGateway): ?PayoutOffer
