a14a6dd9e8d68868f6b1ca11fa3ec4837ef12298
Niiikkid <niiikkid@gmail.com>
2025-01-11 02:04:08 +1000

    fixes

--- app/Http/Requests/API/Payout/StoreRequest.php
+++ app/Http/Requests/API/Payout/StoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\API\Payout;
 
 use App\Enums\DetailType;
+use App\Models\PaymentGateway;
 use App\Models\PayoutGateway;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -25,7 +26,7 @@ class StoreRequest extends FormRequest
      */
     public function rules(): array
     {
-        $payoutGateway = PayoutGateway::where('id', $this->payout_gateway_id)->first();
+        $payoutGateway = PayoutGateway::where('uuid', $this->payout_gateway_id)->first();
 
         $detailRules = ['required', 'string', 'min:3', 'max:30'];
 
@@ -35,6 +36,21 @@ class StoreRequest extends FormRequest
 
         if ($this->detail_type === DetailType::PHONE->value) {
             $detailRules = ['required', 'starts_with:7', 'phone:RU'];
+        }
+
+        $paymentGateway = PaymentGateway::where('code', $this->payment_gateway)->first();
+
+        $subPaymentGateways = [];
+        if ($paymentGateway && $paymentGateway->sub_payment_gateways) {
+            $subPaymentGateways = $paymentGateway->sub_payment_gateways->pluck('code')->toArray();
+        }
+
+        if ($paymentGateway) { //TODO
+            if ($paymentGateway->sub_payment_gateways) {
+                $detailTypes = ['phone'];
+            } else {
+                $detailTypes = ['card'];
+            }
         }
 
         return [
@@ -47,11 +63,11 @@ class StoreRequest extends FormRequest
                 }),
             ],
             'detail' => $detailRules,
-            'detail_type' => ['required', 'in:card,phone'],
+            'detail_type' => ['required', Rule::in($detailTypes)],
             'detail_initials' => ['required', 'string', 'min:3', 'max:30'],
             'amount' => ['required', 'integer', 'min:1'],
             'payment_gateway' => ['required', 'exists:payment_gateways,code'],
-            'sub_payment_gateway' => ['nullable', 'exists:payment_gateways,code'], //TODO validation
+            'sub_payment_gateway' => ['nullable', 'exists:payment_gateways,code', Rule::in($subPaymentGateways)],
             'callback_url' => ['nullable', 'string', 'url:https', 'max:256'],
         ];
     }
