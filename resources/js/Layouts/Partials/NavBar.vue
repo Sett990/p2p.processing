346d2ec0e2fae373c92e26963ca1fe753cdd3410
Niiikkid <niiikkid@gmail.com>
2025-01-13 06:29:15 +1000

    fix

--- resources/js/Layouts/Partials/NavBar.vue
+++ resources/js/Layouts/Partials/NavBar.vue
@@ -56,7 +56,7 @@ router.on('success', (event) => {
             <div class="flex items-center justify-between">
                 <div class="flex items-center justify-start rtl:justify-end">
                     <!--data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"-->
-                    <button type="button" class="inline-flex items-center p-2 pl-0 text-sm text-gray-500 rounded-xl  lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                    <button type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-xl  lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                         <span class="sr-only">Open sidebar</span>
                         <svg class="sm:w-8 sm:h-8 w-7 h-7" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
@@ -92,7 +92,7 @@ router.on('success', (event) => {
                         </span>
                     </div>
                     <div class="flex items-center">
-                        <div id="dropdown-user-button" data-dropdown-toggle="dropdown-user" class="flex items-center space-x-4 cursor-pointer dark:hover:bg-gray-800/75 py-2 px-4 pr-0 rounded-xl">
+                        <div id="dropdown-user-button" data-dropdown-toggle="dropdown-user" class="flex items-center space-x-4 cursor-pointer dark:hover:bg-gray-800/75 py-2 px-4 pr-2 rounded-xl">
                             <div class="flex text-sm bg-gray-400 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
                                 <span class="sr-only">Open user menu</span>
                                 <img :src="'https://api.dicebear.com/8.x/adventurer/svg?seed='+$page.props.auth.user.email" class="w-12 h-12 rounded-full" alt="user photo">
