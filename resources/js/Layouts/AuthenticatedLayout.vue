6467fc587872f368e4d0a2134f80f90158fa1407
Niiikkid <niiikkid@gmail.com>
2025-01-15 20:42:59 +1000

    sidebar

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -68,6 +68,64 @@ const openDocs = () => {
 
 <template>
     <div>
+        <!-- drawer component -->
+        <div id="mobile-sidebar" class="block lg:hidden fixed top-0 left-0 z-50 w-[21rem] h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white dark:bg-gray-800" tabindex="-1" aria-labelledby="mobile-sidebar-label">
+            <h5 id="mobile-sidebar-label" class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400 px-1">Меню</h5>
+            <button type="button" data-drawer-hide="mobile-sidebar" aria-controls="mobile-sidebar" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close menu</span>
+            </button>
+            <div class="py-4 overflow-y-auto flex justify-center">
+                <div class="h-full z-40 space-y-6" aria-label="Sidebar">
+                    <div v-if="userStore.isAdmin" class="p-2 overflow-y-auto bg-white dark:bg-gray-800 w-72 rounded-menu">
+                        <ViewModeSwitcher/>
+                    </div>
+                    <div
+                        v-show="viewStore.isTraderViewMode"
+                        class="p-2 overflow-y-auto bg-white dark:bg-gray-800 w-72 rounded-menu"
+                    >
+                        <OnlineSwitcher/>
+                    </div>
+                    <div class="p-2 overflow-y-auto bg-white dark:bg-gray-800 w-72 rounded-menu">
+                        <TraderMenu
+                            v-show="viewStore.isTraderViewMode"
+                        />
+                        <MerchantMenu
+                            v-show="viewStore.isMerchantViewMode"
+                        />
+                        <AdminMenu
+                            v-show="viewStore.isAdminViewMode"
+                        />
+                    </div>
+                    <div class="p-2 overflow-y-auto bg-white dark:bg-gray-800 w-72 rounded-menu">
+                        <div>
+                            <div class="flex items-center mb-1">
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Курс Tether TRC-20</span>
+                            </div>
+                            <div class="text-sm text-blue-800 dark:text-blue-400">
+                                <ul>
+                                    <li v-for="(rate, index) in rates" v-show="index < 3 || showAllRates" class="flex justify-between items-end border-b border-gray-500 border-dotted last:border-none">
+                                        <span class="text-sm mt-1 text-gray-700 dark:text-gray-200 mr-1.5">{{ rate.buy_price }}</span>
+                                        <span class="text-sm text-blue-500 dark:text-blue-500">{{ rate.code.toUpperCase() }}</span>
+                                    </li>
+                                </ul>
+                                <div class="flex justify-center mt-3">
+                                <span @click="showAllRates = !showAllRates" class="cursor-pointer px-5">
+                                    <span v-show="! showAllRates" class="text-gray-700 dark:text-gray-500 dark:hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        Показать все
+                                    </span>
+                                    <span v-show="showAllRates" class="text-gray-700 dark:text-gray-500 dark:hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        Спрятать
+                                    </span>
+                                </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900 pt-10">
             <div class="container px-3 lg:px-10 mx-auto mb-5">
                 <NavBar/>
