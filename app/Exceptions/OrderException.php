72d13872fc305bb1f7bf89533cb8f41c4e21b579
Niiikkid <niiikkid@gmail.com>
2025-01-25 22:49:57 +1000

    order service

--- app/Exceptions/OrderException.php
+++ app/Exceptions/OrderException.php
@@ -4,5 +4,13 @@ namespace App\Exceptions;
 
 class OrderException extends BaseException
 {
-    //
+    public static function orderAlreadyFinished($order)
+    {
+        return new self("Сделка уже завершена. Order ID: $order->id");
+    }
+
+    public static function orderAlreadyOpened($order)
+    {
+        return new self("Сделка уже открыта. Order ID: $order->id");
+    }
 }
--- app/Services/Order/Features/OrderOperator.php
+++ app/Services/Order/Features/OrderOperator.php
@@ -16,80 +16,85 @@ class OrderOperator
 
     public function finishOrderAsSuccessful(): true
     {
-        if ($this->order->status->equals(OrderStatus::PENDING)) {
-            DB::transaction(function () {
-                $this->order->update([
-                    'status' => OrderStatus::SUCCESS,
-                    'finished_at' => now()
-                ]);
-
-                $this->order->merchant->user->wallet->giveToMerchant(
-                    amount: $this->order->merchant_profit,
-                    type: TransactionType::INCOME_FROM_A_SUCCESSFUL_ORDER
-                );
-            });
-        } else {
-            throw OrderException::make('Cant succeed not pending order');
+        if ($this->order->status->notEquals(OrderStatus::PENDING)) {
+            throw OrderException::orderAlreadyFinished($this->order);
         }
+
+        DB::transaction(function () {
+            $this->order->update([
+                'status' => OrderStatus::SUCCESS,
+                'finished_at' => now()
+            ]);
+
+            //TODO move to listeners
+            $this->order->merchant->user->wallet->giveToMerchant(
+                amount: $this->order->merchant_profit,
+                type: TransactionType::INCOME_FROM_A_SUCCESSFUL_ORDER
+            );
+        });
 
         return true;
     }
 
     public function finishOrderAsFailed(TransactionType $transactionType): true
     {
-        if ($this->order->status->equals(OrderStatus::PENDING)) {
-            DB::transaction(function () use ($transactionType) {
-                $this->order->update([
-                    'status' => OrderStatus::FAIL,
-                    'finished_at' => now()
-                ]);
-
-                $this->order->paymentDetail->user->wallet->giveToTrust(
-                    amount: $this->order->profit,
-                    type: $transactionType
-                );
-
-                (new DailyLimit(
-                    paymentDetail: $this->order->paymentDetail,
-                    amount: $this->order->amount
-                ))->decrement();
-            });
-        } else {
-            throw OrderException::make('Cant fail not pending order');
+        if ($this->order->status->notEquals(OrderStatus::PENDING)) {
+            throw OrderException::orderAlreadyFinished($this->order);
         }
+
+        DB::transaction(function () use ($transactionType) {
+            $this->order->update([
+                'status' => OrderStatus::FAIL,
+                'finished_at' => now()
+            ]);
+
+            //TODO move to listeners
+            $this->order->paymentDetail->user->wallet->giveToTrust(
+                amount: $this->order->profit,
+                type: $transactionType
+            );
+
+            //TODO move to listeners
+            (new DailyLimit(
+                paymentDetail: $this->order->paymentDetail,
+                amount: $this->order->amount
+            ))->decrement();
+        });
 
         return true;
     }
 
     public function reopenFinishedOrder(TransactionType $transactionType): bool
     {
-        if ($this->order->status->notEquals(OrderStatus::PENDING)) {
-            DB::transaction(function () use ($transactionType) {
-                if ($this->order->status->equals(OrderStatus::FAIL)) {
-                    $this->order->paymentDetail->user->wallet->takeFromTrust(
-                        amount: $this->order->profit,
-                        type: $transactionType
-                    );
-                    (new DailyLimit(
-                        paymentDetail: $this->order->paymentDetail,
-                        amount: $this->order->amount
-                    ))->increment();
-                }
-                if ($this->order->status->equals(OrderStatus::SUCCESS)) {
-                    $this->order->merchant->user->wallet->takeFromMerchant(
-                        amount: $this->order->merchant_profit,
-                        type: TransactionType::ROLLBACK_INCOME_FROM_A_SUCCESSFUL_ORDER
-                    );
-                }
-
-                $this->order->update([
-                    'status' => OrderStatus::PENDING,
-                    'finished_at' => null
-                ]);
-            });
-        } else {
-            throw OrderException::make('Cant rollback order');
+        if ($this->order->status->equals(OrderStatus::PENDING)) {
+            throw OrderException::orderAlreadyOpened($this->order);
         }
+
+        DB::transaction(function () use ($transactionType) {
+            //TODO move to listeners
+            if ($this->order->status->equals(OrderStatus::FAIL)) {
+                $this->order->paymentDetail->user->wallet->takeFromTrust(
+                    amount: $this->order->profit,
+                    type: $transactionType
+                );
+                (new DailyLimit(
+                    paymentDetail: $this->order->paymentDetail,
+                    amount: $this->order->amount
+                ))->increment();
+            }
+            //TODO move to listeners
+            if ($this->order->status->equals(OrderStatus::SUCCESS)) {
+                $this->order->merchant->user->wallet->takeFromMerchant(
+                    amount: $this->order->merchant_profit,
+                    type: TransactionType::ROLLBACK_INCOME_FROM_A_SUCCESSFUL_ORDER
+                );
+            }
+
+            $this->order->update([
+                'status' => OrderStatus::PENDING,
+                'finished_at' => null
+            ]);
+        });
 
         return true;
     }
