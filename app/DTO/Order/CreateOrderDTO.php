64cc21be1d2e143c4a0bdbb14194233eb89fa8c8
Niiikkid <niiikkid@gmail.com>
2025-02-01 14:44:24 +1000

    fix

--- app/DTO/Order/CreateOrderDTO.php
+++ app/DTO/Order/CreateOrderDTO.php
@@ -41,7 +41,7 @@ readonly class CreateOrderDTO extends BaseDTO
             $data['merchant'] = Merchant::where('uuid', $data['merchant_id'])->first();
         }
 
-        if ($data['sub_payment_gateway'] === 0) {
+        if (! empty($data['sub_payment_gateway']) && $data['sub_payment_gateway'] === 0) {
             $data['sub_payment_gateway'] = null;
         }
 
