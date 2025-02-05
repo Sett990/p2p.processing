900b683dcc2703cc91d854aee9eef807959d66bd
Niiikkid <niiikkid@gmail.com>
2025-02-05 13:06:51 +1000

    parsing

--- app/Services/Sms/Parser.php
+++ app/Services/Sms/Parser.php
@@ -75,6 +75,7 @@ class Parser
             'поступление',
             'пополнение',
             'зачисление',
+            'зачислено',
             '[а-я]+\sпополнена',
             'popolnenie scheta',
             'postuplenie sredstv na schet',
@@ -155,7 +156,7 @@ class Parser
 
     public function parseCardLastDigitsFromMessage(string $message): ?string
     {
-        $regex = '(\*|счёт|mir-|ecmc|\s••\s|\s\d{6}\.\.|карта\s\*\*\*\s)(?<card_last_digits>\d{4})(\D|$)';
+        $regex = '(\*|счёт|mir-|ecmc|\s••\s|\s\d{6}\.\.|карта\s\*\*\*\s|^карта\s)(?<card_last_digits>\d{4})(\D|$)';
 
         $regex = '/' . $regex . '/mi';
         preg_match_all($regex, $message, $matches, PREG_SET_ORDER);
