655a1b3377e4d6661765c7e2c6dd61df26daddac
Niiikkid <niiikkid@gmail.com>
2025-02-05 14:27:16 +1000

    filters

--- resources/js/Components/Filters/Pertials/InputFilter.vue
+++ resources/js/Components/Filters/Pertials/InputFilter.vue
@@ -11,13 +11,13 @@ const model = defineModel({
 </script>
 
 <template>
-    <div>
+    <div class="w-48">
         <input
             type="text"
             :id="$.uid"
             v-model="model"
             :placeholder="placeholder"
-            class="w-48 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block p-2.5 h-[38px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block p-2.5 h-[38px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
     </div>
 </template>
