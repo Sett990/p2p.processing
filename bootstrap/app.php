3894e36302c8b7611e30bd41184e7f4ade29a35e
Niiikkid <niiikkid@gmail.com>
2025-02-11 21:40:53 +1000

    sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,7 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        \Sentry\Laravel\Integration::handles($exceptions);
     })
     ->withEvents(discover: [
         __DIR__.'/../app/Listeners',
