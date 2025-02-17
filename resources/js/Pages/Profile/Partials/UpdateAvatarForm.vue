19c6b80c242fc8a9cde97bea58e7d88bab682f80
Niiikkid <niiikkid@gmail.com>
2025-02-18 07:26:13 +1000

    fix

--- resources/js/Pages/Profile/Partials/UpdateAvatarForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateAvatarForm.vue
@@ -76,7 +76,7 @@ const avatarsStyles = [
                     class="px-0 py-0 text-blue-500 hover:text-blue-600 inline-flex items-center hover:underline"
                 >
                     <svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 10 3-3m0 0-3-3m3 3H5v3m3 4-3 3m0 0 3 3m-3-3h14v-3"/>
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
                     </svg>
                 </a>
             </div>
