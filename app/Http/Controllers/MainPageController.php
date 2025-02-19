4351258b08e8b2a5b9567e2e6e39cbcd46634695
Niiikkid <niiikkid@gmail.com>
2025-02-20 09:31:06 +1000

    fix

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -17,64 +17,68 @@ class MainPageController extends Controller
 {
     public function merchant()
     {
-        $query = Order::query()
-            ->whereRelation('merchant', 'user_id', auth()->id())
-            ->where('status', OrderStatus::SUCCESS);
+        $stats = cache()->remember('merchant-main-page-stats-'.auth()->id(), 60 * 60, function () {
+            $query = Order::query()
+                ->whereRelation('merchant', 'user_id', auth()->id())
+                ->where('status', OrderStatus::SUCCESS);
 
-        $totalProfit = Money::fromUnits($query->clone()->sum('merchant_profit'), Currency::USDT());
+            $totalProfit = Money::fromUnits($query->clone()->sum('merchant_profit'), Currency::USDT());
 
-        $totalWithdrawalAmount = Invoice::query()
-            ->whereRelation('wallet', 'user_id', auth()->id())
-            ->where('type', InvoiceType::WITHDRAWAL)
-            ->where('balance_type', BalanceType::MERCHANT)
-            ->where('status', InvoiceStatus::SUCCESS)
-            ->sum('amount');
-        $totalWithdrawalAmount = Money::fromUnits($totalWithdrawalAmount, Currency::USDT());
+            $totalWithdrawalAmount = Invoice::query()
+                ->whereRelation('wallet', 'user_id', auth()->id())
+                ->where('type', InvoiceType::WITHDRAWAL)
+                ->where('balance_type', BalanceType::MERCHANT)
+                ->where('status', InvoiceStatus::SUCCESS)
+                ->sum('amount');
+            $totalWithdrawalAmount = Money::fromUnits($totalWithdrawalAmount, Currency::USDT());
 
-        $balance = services()->wallet()->getTotalAvailableBalance(auth()->user()->wallet, BalanceType::MERCHANT);
+            $balance = services()->wallet()->getTotalAvailableBalance(auth()->user()->wallet, BalanceType::MERCHANT);
 
-        $successOrderCount = $query->clone()->count();
+            $successOrderCount = $query->clone()->count();
 
-        //=====
+            //=====
 
-        // Получаем текущий месяц и год
-        $startOfMonth = now()->startOfMonth();
-        $endOfMonth = now()->endOfMonth();
+            // Получаем текущий месяц и год
+            $startOfMonth = now()->startOfMonth();
+            $endOfMonth = now()->endOfMonth();
 
-        // Запрос для получения суммы доходов по дням
-        $earningsByDay = Order::where('status', OrderStatus::SUCCESS)
-            ->whereRelation('merchant', 'user_id', auth()->id())
-            ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
-            ->selectRaw('DATE(created_at) as date, SUM(merchant_profit) as total_earnings')
-            ->groupBy('date')
-            ->orderBy('date')
-            ->get();
+            // Запрос для получения суммы доходов по дням
+            $earningsByDay = Order::where('status', OrderStatus::SUCCESS)
+                ->whereRelation('merchant', 'user_id', auth()->id())
+                ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
+                ->selectRaw('DATE(created_at) as date, SUM(merchant_profit) as total_earnings')
+                ->groupBy('date')
+                ->orderBy('date')
+                ->get();
 
-        // Формируем данные для графика
-        $labels = [];
-        $data = [];
+            // Формируем данные для графика
+            $labels = [];
+            $data = [];
 
-        // Определяем текущую дату и дату 30 дней назад
-        $startDate = now()->subDays(29); // Дата 30 дней назад (включая текущий день)
+            // Определяем текущую дату и дату 30 дней назад
+            $startDate = now()->subDays(29); // Дата 30 дней назад (включая текущий день)
 
-        // Заполняем данные для каждого из последних 30 дней
-        for ($i = 0; $i < 30; $i++) {
-            $date = $startDate->copy()->addDays($i);
-            $labels[] = $date->day; // Форматируем дату для отображения
-            $data[] = $earningsByDay->firstWhere('date', $date->toDateString())->total_earnings ?? 0;
-        }
+            // Заполняем данные для каждого из последних 30 дней
+            for ($i = 0; $i < 30; $i++) {
+                $date = $startDate->copy()->addDays($i);
+                $labels[] = $date->day; // Форматируем дату для отображения
+                $data[] = $earningsByDay->firstWhere('date', $date->toDateString())->total_earnings ?? 0;
+            }
 
-        return Inertia::render('MainPage/Merchant/Index', [
-            'statistics' => [
-                'totalProfit' => $totalProfit->toBeauty(),
-                'totalWithdrawalAmount' => $totalWithdrawalAmount->toBeauty(),
-                'balance' => $balance->toBeauty(),
-                'successOrderCount' => $successOrderCount,
-            ],
-            'chart' => [
-                'labels' => $labels,
-                'data' => $data,
-            ]
-        ]);
+            return [
+                'statistics' => [
+                    'totalProfit' => $totalProfit->toBeauty(),
+                    'totalWithdrawalAmount' => $totalWithdrawalAmount->toBeauty(),
+                    'balance' => $balance->toBeauty(),
+                    'successOrderCount' => $successOrderCount,
+                ],
+                'chart' => [
+                    'labels' => $labels,
+                    'data' => $data,
+                ]
+            ];
+        });
+
+        return Inertia::render('MainPage/Merchant/Index', $stats);
     }
 }
