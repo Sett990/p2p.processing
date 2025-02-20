6d199c458dc78993160c9806963b0af05cfb566a
Niiikkid <niiikkid@gmail.com>
2025-02-20 14:10:16 +1000

    fix

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -112,7 +112,8 @@ defineOptions({ layout: AuthenticatedLayout })
                                 {{ user.id }}
                             </th>
                             <td class="px-6 py-3 text-nowrap">
-                                <div class="inline-flex">
+                                <div class="inline-flex items-center gap-2">
+                                    <img :src="'https://api.dicebear.com/9.x/'+user.avatar_style+'/svg?seed='+user.avatar_uuid" class="w-10 h-10 rounded-full" alt="user photo">
                                     <div>
                                         <div class="text-nowrap text-gray-900 dark:text-gray-200">
                                             {{ user.email }}
@@ -123,7 +124,6 @@ defineOptions({ layout: AuthenticatedLayout })
                                     </div>
                                     <span
                                         v-if="user.banned_at"
-                                        class="ml-2"
                                     >
                                         <svg class="w-4 h-4 text-red-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                             <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z" clip-rule="evenodd"/>
