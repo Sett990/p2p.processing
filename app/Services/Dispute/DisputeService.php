4e6d3fe424113b11dfdd6b96daa88f5c806b7b68
Niiikkid <niiikkid@gmail.com>
2025-02-26 14:05:50 +1000

    fix

--- app/Services/Dispute/DisputeService.php
+++ app/Services/Dispute/DisputeService.php
@@ -37,7 +37,7 @@ class DisputeService implements DisputeServiceContract
         $dispute = Dispute::create([
             'receipt' => $receipt_name,
             'order_id' => $order->id,
-            'user_id' => $order->paymentDetail->user_id,
+            'trader_id' => $order->paymentDetail->user_id,
             'status' => DisputeStatus::PENDING,
         ]);
 
