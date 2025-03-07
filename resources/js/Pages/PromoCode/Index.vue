e73aac79e9d36b54457faacc481d63cfe4cdfec5
Niiikkid <niiikkid@gmail.com>
2025-03-07 16:22:55 +1000

    promocodes

---
+++ resources/js/Pages/PromoCode/Index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {Head, router, usePage} from '@inertiajs/vue3';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import MainTableSection from "@/Wrappers/MainTableSection.vue";
+import {ref} from "vue";
+
+const promoCodes = ref(usePage().props.promoCodes);
+const filters = ref(usePage().props.filters);
+
+router.on('success', (event) => {
+    promoCodes.value = usePage().props.promoCodes;
+})
+
+defineOptions({ layout: AuthenticatedLayout })
+</script>
+
+<template>
+    <div>
+        <Head title="Промокоды" />
+
+        <MainTableSection
+            title="Промокоды"
+            :data="promoCodes"
+            :query-data="{filters}"
+        >
+            <template v-slot:header>
+
+            </template>
+            <template v-slot:body>
+
+            </template>
+        </MainTableSection>
+    </div>
+</template>
