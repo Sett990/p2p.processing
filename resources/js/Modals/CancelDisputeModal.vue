e2ac26b05c10079755e77cfb0dda9c0b6bb8cf09
Niiikkid <niiikkid@gmail.com>
2025-03-07 21:11:41 +1000

    fix

--- resources/js/Modals/CancelDisputeModal.vue
+++ resources/js/Modals/CancelDisputeModal.vue
@@ -71,6 +71,7 @@ const cancel = (dispute) => {
             </button>
             <button
                 @click.prevent="cancel(disputeCancelModal.params.dispute)"
+                :disabled="form.processing"
                 type="button"
                 class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl  text-sm px-5 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
             >
