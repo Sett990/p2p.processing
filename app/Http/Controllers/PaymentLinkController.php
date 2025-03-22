d575893a567d69812ff45a225d094098d502c522
Niiikkid <niiikkid@gmail.com>
2025-03-22 14:12:25 +1000

    fix

--- app/Http/Controllers/PaymentLinkController.php
+++ app/Http/Controllers/PaymentLinkController.php
@@ -51,7 +51,7 @@ class PaymentLinkController extends Controller
             'detail' => $order->paymentDetail?->detail,
             'detail_type' => $order->paymentDetail?->detail_type->value,
             'initials' => $order->paymentDetail?->initials,
-            'payment_gateway' => $order->paymentDetail?->paymentGateway?->name,//TODO не работает
+            'payment_gateway' => $order->paymentGateway->name,
             'success_url' => $order->success_url,
             'fail_url' => $order->fail_url,
             'created_at' => $order->created_at->toDateTimeString(),
