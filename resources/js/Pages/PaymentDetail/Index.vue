05ce2a4334530132b6d9cb3f7f2527f6a1cebc0b
Niiikkid <niiikkid@gmail.com>
2025-04-13 15:22:35 +1000

    orders

--- resources/js/Pages/PaymentDetail/Index.vue
+++ resources/js/Pages/PaymentDetail/Index.vue
@@ -253,20 +253,20 @@ defineOptions({ layout: AuthenticatedLayout })
                                     class="px-6 py-3"
                                 >
                                     <div class="space-y-1">
-                                        <div class="flex items-center gap-2 text-nowrap">
-                                            <svg class="w-5 h-5 text-blue-500 transition duration-75 dark:text-blue-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15h12M6 6h12m-6 12h.01M7 21h10a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1Z"/>
-                                            </svg>
-                                            <span>{{ payment_detail.device_name }}</span>
-                                        </div>
                                         <div
                                             v-if="viewStore.isAdminViewMode"
                                             class="flex items-center gap-2 text-nowrap"
                                         >
-                                            <svg class="w-5 h-5 text-blue-500 transition duration-75 dark:text-blue-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                                <path stroke="currentColor" stroke-width="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                            <svg class="w-5 h-5 text-blue-500 transition dark:text-blue-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-width="1.5" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
                                             </svg>
                                             <span>{{ payment_detail.owner_name }}</span>
+                                        </div>
+                                        <div class="flex items-center gap-2 text-nowrap">
+                                            <svg class="w-4 h-4 ml-0.5 mr-0.5 text-blue-500 transition dark:text-blue-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15h12M6 6h12m-6 12h.01M7 21h10a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1Z"/>
+                                            </svg>
+                                            <span>{{ payment_detail.device_name }}</span>
                                         </div>
                                     </div>
                                 </td>
