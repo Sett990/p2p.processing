44a36b789d31ae10208cdb4eaa0858e62e8d1a71
Niiikkid <niiikkid@gmail.com>
2025-04-16 13:10:35 +1000

    fix

--- app/Services/Order/Features/OrderDetailProvider/Classes/GatewaysProvider.php
+++ app/Services/Order/Features/OrderDetailProvider/Classes/GatewaysProvider.php
@@ -9,7 +9,6 @@ use App\Services\Money\Currency;
 use App\Services\Money\Money;
 use App\Services\Order\Features\OrderDetailProvider\Values\Gateway;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
 
 class GatewaysProvider
 {
@@ -28,91 +27,76 @@ class GatewaysProvider
      */
     public function get(): Collection
     {
-        $cacheKey = $this->makeCacheKey();
-        return cache()->remember($cacheKey, now()->addSeconds(15), function () {
-            if ($this->gateway) {
-                $paymentGateways = PaymentGateway::query()
-                    ->where(function ($query) {
-                        $query->where('min_limit', '<=', intval($this->amount->toBeauty()));
-                        $query->where('max_limit', '>=', intval($this->amount->toBeauty()));
-                    })
-                    ->where('code', $this->gateway->code)
-                    ->active()
-                    ->get();
-            } else if ($this->currency) {
-                $paymentGateways = PaymentGateway::query()
-                    ->where(function ($query) {
-                        $query->where('min_limit', '<=', intval($this->amount->toBeauty()));
-                        $query->where('max_limit', '>=', intval($this->amount->toBeauty()));
-                    })
-                    ->where('currency', $this->currency->getCode())
-                    ->where('is_intrabank', false)
-                    ->active()
-                    ->get();
-            } else {
-                throw OrderException::make('Требуется валюта или платежный метод.');
-            }
+        if ($this->gateway) {
+            $paymentGateways = PaymentGateway::query()
+                ->where(function ($query) {
+                    $query->where('min_limit', '<=', intval($this->amount->toBeauty()));
+                    $query->where('max_limit', '>=', intval($this->amount->toBeauty()));
+                })
+                ->where('code', $this->gateway->code)
+                ->active()
+                ->get();
+        } else if ($this->currency) {
+            $paymentGateways = PaymentGateway::query()
+                ->where(function ($query) {
+                    $query->where('min_limit', '<=', intval($this->amount->toBeauty()));
+                    $query->where('max_limit', '>=', intval($this->amount->toBeauty()));
+                })
+                ->where('currency', $this->currency->getCode())
+                ->where('is_intrabank', false)
+                ->active()
+                ->get();
+        } else {
+            throw OrderException::make('Требуется валюта или платежный метод.');
+        }
 
-            if ($paymentGateways->isEmpty()) {
-                throw OrderException::make('Подходящий платежный метод не найден для данных лимитов/валюты.');
-            }
+        if ($paymentGateways->isEmpty()) {
+            throw OrderException::make('Подходящий платежный метод не найден для данных лимитов/валюты.');
+        }
 
-            $gatewaySettings = $this->merchant->gateway_settings;
+        $gatewaySettings = $this->merchant->gateway_settings;
 
-            $paymentGateways = $paymentGateways->filter(function (PaymentGateway $paymentGateway) use ($gatewaySettings) {
-                return $gatewaySettings[$paymentGateway->id]['active'] ?? true;
-            });
-
-            if ($paymentGateways->isEmpty()) {
-                throw OrderException::make('Подходящий платежный метод не найден для данных лимитов/валюты.');
-            }
-
-            $gateways = collect();
-
-            $paymentGateways->each(function (PaymentGateway $paymentGateway) use (&$gateways) {
-                $customGatewaySettings = null;
-                if (! empty($this->merchant->gateway_settings[$paymentGateway->id])) {
-                    $customGatewaySettings = $this->merchant->gateway_settings[$paymentGateway->id];
-                }
-
-                $serviceCommissionRateTotal = $paymentGateway->total_service_commission_rate_for_orders;
-
-                if (isset($customGatewaySettings['custom_gateway_commission']) && $customGatewaySettings['custom_gateway_commission'] > 0) {
-                    $serviceCommissionRateTotal = $customGatewaySettings['custom_gateway_commission'];
-                } else if (isset($customGatewaySettings['custom_gateway_commission']) && (int)$customGatewaySettings['custom_gateway_commission'] === 0) {
-                    $serviceCommissionRateTotal = 0;
-                }
-
-                if (! empty($customGatewaySettings['custom_gateway_reservation_time'])) {
-                    $reservationTime = (int)$customGatewaySettings['custom_gateway_reservation_time'];
-                } else {
-                    $reservationTime = $paymentGateway->reservation_time_for_orders;
-                }
-
-                $gateways->push(
-                    new Gateway(
-                        id: $paymentGateway->id,
-                        code: $paymentGateway->code,
-                        reservationTime: $reservationTime,
-                        serviceCommissionRate: $serviceCommissionRateTotal,
-                        traderCommissionRate: $paymentGateway->trader_commission_rate_for_orders,
-                    )
-                );
-            });
-
-            return $gateways;
+        $paymentGateways = $paymentGateways->filter(function (PaymentGateway $paymentGateway) use ($gatewaySettings) {
+            return $gatewaySettings[$paymentGateway->id]['active'] ?? true;
         });
-    }
 
-    /**
-     * Генерирует уникальный ключ кэша на основе параметров конструктора
-     */
-    protected function makeCacheKey(): string
-    {
-        $merchantId = $this->merchant->id;
-        $amount = $this->amount->toBeauty();
-        $currency = $this->currency?->getCode() ?? 'null';
-        $gateway = $this->gateway?->code ?? 'null';
-        return 'gateways_provider_' . md5(implode('|', [$merchantId, $amount, $currency, $gateway]));
+        if ($paymentGateways->isEmpty()) {
+            throw OrderException::make('Подходящий платежный метод не найден для данных лимитов/валюты.');
+        }
+
+        $gateways = collect();
+
+        $paymentGateways->each(function (PaymentGateway $paymentGateway) use (&$gateways) {
+            $customGatewaySettings = null;
+            if (! empty($this->merchant->gateway_settings[$paymentGateway->id])) {
+                $customGatewaySettings = $this->merchant->gateway_settings[$paymentGateway->id];
+            }
+
+            $serviceCommissionRateTotal = $paymentGateway->total_service_commission_rate_for_orders;
+
+            if (isset($customGatewaySettings['custom_gateway_commission']) && $customGatewaySettings['custom_gateway_commission'] > 0) {
+                $serviceCommissionRateTotal = $customGatewaySettings['custom_gateway_commission'];
+            } else if (isset($customGatewaySettings['custom_gateway_commission']) && (int)$customGatewaySettings['custom_gateway_commission'] === 0) {
+                $serviceCommissionRateTotal = 0;
+            }
+
+            if (! empty($customGatewaySettings['custom_gateway_reservation_time'])) {
+                $reservationTime = (int)$customGatewaySettings['custom_gateway_reservation_time'];
+            } else {
+                $reservationTime = $paymentGateway->reservation_time_for_orders;
+            }
+
+            $gateways->push(
+                new Gateway(
+                    id: $paymentGateway->id,
+                    code: $paymentGateway->code,
+                    reservationTime: $reservationTime,
+                    serviceCommissionRate: $serviceCommissionRateTotal,
+                    traderCommissionRate: $paymentGateway->trader_commission_rate_for_orders,
+                )
+            );
+        });
+
+        return $gateways;
     }
 }
