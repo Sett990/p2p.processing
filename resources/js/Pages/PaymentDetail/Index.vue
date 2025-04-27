6204dc032b9f22935843253f78445b4592917a87
Niiikkid <niiikkid@gmail.com>
2025-04-28 01:57:50 +1000

    fix

--- resources/js/Pages/PaymentDetail/Index.vue
+++ resources/js/Pages/PaymentDetail/Index.vue
@@ -290,30 +290,27 @@ defineOptions({ layout: AuthenticatedLayout })
                                                 {{payment_detail.max_pending_orders_quantity}}
                                             </span>
                                         </div>
-
-                                        <div v-if="payment_detail.pending_orders_count / payment_detail.max_pending_orders_quantity >= 0.8"
-                                             class="animate-pulse">
-                                            <svg class="w-5 h-5 text-red-500 dark:text-red-400"
-                                                 fill="currentColor"
-                                                 viewBox="0 0 20 20"
-                                                 xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd"
-                                                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                                                      clip-rule="evenodd">
-                                                </path>
+<!--                                        <div v-if="payment_detail.pending_orders_count / payment_detail.max_pending_orders_quantity >= 0.8" class="animate-pulse">
+                                            <svg class="w-5 h-5 text-red-500 dark:text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <rect x="2" y="7" width="16" height="10" rx="2" stroke="currentColor" stroke-width="2"/>
+                                                <rect x="4" y="9" width="12" height="6" fill="currentColor" stroke="none"/>
+                                                <rect x="18" y="10" width="2" height="4" rx="0.5" stroke="currentColor" stroke-width="2"/>
                                             </svg>
                                         </div>
                                         <div v-else-if="payment_detail.pending_orders_count / payment_detail.max_pending_orders_quantity >= 0.5">
-                                            <svg class="w-5 h-5 text-yellow-500 dark:text-yellow-400"
-                                                 fill="currentColor"
-                                                 viewBox="0 0 20 20"
-                                                 xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd"
-                                                      d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                                                      clip-rule="evenodd">
-                                                </path>
+                                            <svg class="w-5 h-5 text-yellow-500 dark:text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <rect x="2" y="7" width="16" height="10" rx="2" stroke="currentColor" stroke-width="2"/>
+                                                <rect x="4" y="11" width="6" height="2" fill="currentColor" stroke="none"/>
+                                                <rect x="18" y="10" width="2" height="4" rx="0.5" stroke="currentColor" stroke-width="2"/>
                                             </svg>
                                         </div>
+                                        <div v-else-if="payment_detail.pending_orders_count / payment_detail.max_pending_orders_quantity >= 0">
+                                            <svg class="w-5 h-5 text-green-600 dark:text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <rect x="2" y="7" width="16" height="10" rx="2" stroke="currentColor" stroke-width="2"/>
+                                                <rect x="4" y="11" width="2" height="2" fill="currentColor" stroke="none"/>
+                                                <rect x="18" y="10" width="2" height="4" rx="0.5" stroke="currentColor" stroke-width="2"/>
+                                            </svg>
+                                        </div>-->
                                     </div>
                                 </td>
                                 <td class="px-6 py-3" v-if="viewStore.isAdminViewMode || isVipUser">
