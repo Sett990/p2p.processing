601d7602cdd4d3e1f6a7fed309bc104ee7a04f4c
rezuspay <rezuspay@gmail.com>
2025-10-25 06:34:10 +1000

    fix

--- resources/js/Pages/Referral/Index.vue
+++ resources/js/Pages/Referral/Index.vue
@@ -28,58 +28,67 @@ defineOptions({ layout: AuthenticatedLayout })
             description="Здесь вы можете увидеть список всех ваших рефералов."
         >
             <template v-slot:body>
-                <div class="relative overflow-x-auto shadow-md rounded-table">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <div class="overflow-x-auto card bg-base-100 shadow">
+                    <table class="table table-md">
+                        <thead class="text-xs uppercase bg-base-300">
                             <tr>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="whitespace-nowrap">
                                     ID
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="whitespace-nowrap">
                                     Пользователь
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="whitespace-nowrap">
                                     Промокод
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="whitespace-nowrap">
                                     Сделок
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="whitespace-nowrap">
                                     Доход
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="whitespace-nowrap">
                                     Дата привлечения
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="referral in referrals.data" :key="referral.id" class="bg-white border-b last:border-none dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-3 font-medium whitespace-nowrap text-gray-900 dark:text-gray-200">
+                            <tr v-for="referral in referrals.data" :key="referral.id" class="hover">
+                                <th scope="row" class="font-medium whitespace-nowrap">
                                     {{ referral.id }}
                                 </th>
-                                <td class="px-6 py-3 text-nowrap">
-                                    <div class="inline-flex items-center gap-2">
-                                        <img :src="'https://api.dicebear.com/9.x/'+referral.avatar_style+'/svg?seed='+referral.avatar_uuid" class="w-10 h-10 rounded-full" alt="user photo">
-                                        <div>
-                                            <div class="text-nowrap text-gray-900 dark:text-gray-200">
+                                <td class="whitespace-nowrap">
+                                    <div class="inline-flex items-center gap-3">
+                                        <div class="avatar">
+                                            <div class="w-10 rounded-full">
+                                                <img :src="'https://api.dicebear.com/9.x/'+referral.avatar_style+'/svg?seed='+referral.avatar_uuid" alt="user photo">
+                                            </div>
+                                        </div>
+                                        <div class="leading-tight">
+                                            <div class="whitespace-nowrap">
                                                 {{ referral.email }}
                                             </div>
-                                            <div class="text-nowrap text-xs">
+                                            <div class="text-xs text-base-content/70 whitespace-nowrap">
                                                 {{ referral.name }}
                                             </div>
                                         </div>
                                     </div>
                                 </td>
-                                <td class="px-6 py-3 text-nowrap">
-                                    {{ referral.promo_code?.code || '-' }}
+                                <td class="whitespace-nowrap">
+                                    <span v-if="referral.promo_code?.code" class="badge badge-ghost">
+                                        {{ referral.promo_code.code }}
+                                    </span>
+                                    <span v-else class="badge badge-ghost">-</span>
                                 </td>
-                                <td class="px-6 py-3 text-nowrap">
+                                <td class="whitespace-nowrap">
                                     {{ referral.orders_count }}
                                 </td>
-                                <td class="px-6 py-3 text-nowrap">
-                                    {{ referral.total_profit || '0.00' }} USDT
+                                <td class="whitespace-nowrap">
+                                    <span class="badge badge-outline">
+                                        {{ referral.total_profit || '0.00' }} USDT
+                                    </span>
                                 </td>
-                                <td class="px-6 py-3 text-nowrap">
+                                <td class="whitespace-nowrap">
                                     <DateTime :data="referral.promo_used_at"/>
                                 </td>
                             </tr>
