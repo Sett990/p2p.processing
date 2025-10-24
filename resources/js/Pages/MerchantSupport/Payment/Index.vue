4a7769580fa19bfa8b6c9e5d01a2e67104ccd3de
rezuspay <rezuspay@gmail.com>
2025-10-25 06:24:11 +1000

    such and such

--- resources/js/Pages/MerchantSupport/Payment/Index.vue
+++ resources/js/Pages/MerchantSupport/Payment/Index.vue
@@ -55,88 +55,74 @@ defineOptions({ layout: AuthenticatedLayout })
                 </FiltersPanel>
             </template>
             <template v-slot:body>
-                <div class="relative overflow-x-auto shadow-md sm:rounded-table ">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <div class="overflow-x-auto card bg-base-100 shadow">
+                    <table class="table table-md">
+                        <thead class="text-xs uppercase bg-base-300">
                         <tr>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col">
                                 UUID
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col">
                                 Сумма
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col">
                                 Прибыль
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col">
                                 Комиссия
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col">
                                 Курс
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col">
                                 Статус
                             </th>
-                            <th scope="col" class="px-6 py-3 text-nowrap">
+                            <th scope="col" class="text-nowrap">
                                 Внешний ID
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col">
                                 Создан
                             </th>
                             <th scope="col" class="px-0 py-3"></th>
-                            <th scope="col" class="px-6 py-3 flex justify-center">
+                            <th scope="col" class="flex justify-center">
                                 <span class="sr-only">Действия</span>
                             </th>
                         </tr>
                         </thead>
                         <tbody>
-                        <tr v-for="order in orders.data" class="bg-white border-b last:border-none dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-3 font-medium whitespace-nowrap text-gray-900 dark:text-gray-200">
+                        <tr v-for="order in orders.data">
+                            <th scope="row" class="font-medium whitespace-nowrap text-gray-900 dark:text-gray-200">
                                 <DisplayUUID :uuid="order.uuid"/>
                             </th>
-                            <td class="px-6 py-3">
+                            <td>
                                 <div class="text-nowrap text-gray-900 dark:text-gray-200">{{ order.amount }} {{ order.currency.toUpperCase() }}</div>
                                 <div class="text-nowrap text-xs">{{ order.total_profit }} {{ order.base_currency.toUpperCase() }}</div>
                             </td>
-                            <td class="px-6 py-3">
+                            <td>
                                 <div class="text-nowrap">{{ order.merchant_profit }} {{ order.base_currency.toUpperCase() }}</div>
                             </td>
-                            <td class="px-6 py-3 text-nowrap">
+                            <td class="text-nowrap">
                                 {{ order.service_commission_amount_total }} {{ order.base_currency.toUpperCase() }}
                             </td>
-                            <td class="px-6 py-3">
+                            <td>
                                 {{ order.conversion_price }}
                             </td>
-                            <td class="px-6 py-3">
+                            <td>
                                 <OrderStatus :status="order.status" :status_name="order.status_name"></OrderStatus>
                             </td>
-                            <td class="px-6 py-3">
+                            <td>
                                 {{ order.external_id }}
                             </td>
-                            <td class="px-6 py-3">
+                            <td>
                                 <DateTime class="justify-center" :data="order.created_at"/>
                             </td>
                             <td class="px-0 py-3">
                                 <div>
-                                    <button
-                                        v-if="order.is_h2h"
-                                        @click.prevent="false"
-                                        type="button"
-                                        class="px-2 py-1 text-xs font-medium text-center inline-flex items-center text-gray-900 bg-white border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-100 rounded-xl  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 dark:focus:ring-gray-700"
-                                    >
-                                        H2H
-                                    </button>
-                                    <button
-                                        v-else
-                                        @click.prevent="false"
-                                        type="button"
-                                        class="px-2 py-1 text-xs font-medium text-center inline-flex items-center text-gray-900 bg-white border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-100 rounded-xl  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 dark:focus:ring-gray-700"
-                                    >
-                                        Merchant
-                                    </button>
+                                    <div v-if="order.is_h2h" class="badge badge-outline">H2H</div>
+                                    <div v-else class="badge badge-neutral">Merchant</div>
                                 </div>
                             </td>
-                            <td class="px-6 py-3 text-right">
+                            <td class="text-right">
                                 <TableActionsDropdown>
                                     <TableAction v-if="!order.is_h2h" @click="orderPaymentLink(order.payment_link)">
                                         Платежная страница
