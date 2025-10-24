91a98d02865db789d2ec15cb59e5f26eafa187fa
rezuspay <rezuspay@gmail.com>
2025-10-25 05:44:46 +1000

    such and such

--- resources/js/Pages/Merchant/Tabs/Payments.vue
+++ resources/js/Pages/Merchant/Tabs/Payments.vue
@@ -23,45 +23,45 @@ const currentPage = ref(orders?.meta?.current_page)
 
 <template>
     <div>
-        <h2 class="text-gray-500 text-xs mb-3">Здесь отображаются только оплаченные сделки</h2>
+        <h2 class="text-xs text-base-content/60 mb-3">Здесь отображаются только оплаченные сделки</h2>
 
-        <div class="relative overflow-x-auto shadow-md rounded-table mb-5">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div class="overflow-x-auto card bg-base-100 shadow mb-5">
+            <table class="table table-md">
+                <thead class="text-xs uppercase bg-base-300">
                 <tr>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col">
                         UUID
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col">
                         Сумма
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col">
                         Прибыль
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col">
                         Комиссия
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col">
                         Создан
                     </th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr v-for="order in orders.data" class="bg-white border-b last:border-none dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-3 font-medium whitespace-nowrap text-gray-900 dark:text-gray-200">
+                <tr v-for="order in orders.data" class="hover">
+                    <th scope="row" class="font-medium whitespace-nowrap">
                         <DisplayUUID :uuid="order.uuid"/>
                     </th>
-                    <td class="px-6 py-3">
-                        <div class="text-nowrap text-gray-900 dark:text-gray-200">{{ order.amount }} {{ order.currency.toUpperCase() }}</div>
-                        <div class="text-nowrap text-xs">{{ order.total_profit }} {{ order.base_currency.toUpperCase() }}</div>
+                    <td>
+                        <div class="text-nowrap">{{ order.amount }} {{ order.currency.toUpperCase() }}</div>
+                        <div class="text-nowrap text-xs text-base-content/60">{{ order.total_profit }} {{ order.base_currency.toUpperCase() }}</div>
                     </td>
-                    <td class="px-6 py-3">
+                    <td>
                         <div class="text-nowrap">{{ order.merchant_profit }} {{ order.base_currency.toUpperCase() }}</div>
                     </td>
-                    <td class="px-6 py-3">
+                    <td>
                         {{ order.service_commission_amount_total }} {{ order.base_currency.toUpperCase() }}
                     </td>
-                    <td class="px-6 py-3">
+                    <td>
                         <DateTime class="justify-center" :data="order.created_at"/>
                     </td>
                 </tr>
