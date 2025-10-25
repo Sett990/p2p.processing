83afa00978ba5e72c0516a4bb0e25c2addf8c544
rezuspay <rezuspay@gmail.com>
2025-10-26 02:59:11 +1000

    such and such

--- resources/js/Layouts/Partials/NavBar.vue
+++ resources/js/Layouts/Partials/NavBar.vue
@@ -81,18 +81,22 @@ onMounted(() => {
                     <!--data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"-->
                     <button
                         type="button"
-                        class="inline-flex items-center p-2 text-sm text-gray-500 rounded-xl  lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        class="inline-flex items-center p-2 mr-2 text-sm text-base-content rounded-xl border border-base-content lg:hidden hover:bg-primary hover:text-neutral hover:border-primary focus:outline-none focus:ring-2 focus:ring-gray-200 "
                         @click.prevent="toggleSidebar"
                     >
                         <span class="sr-only">Открыть меню</span>
-                        <svg class="sm:w-8 sm:h-8 w-7 h-7" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="w-10 h-10" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                         </svg>
                     </button>
                     <Link :href="route('dashboard')" class="flex ms-2 md:me-24">
-                        <div class="block">
+                        <div class="hidden lg:block">
                             <div class="text-5xl font-semibold">P2P</div>
                             <div class="text-xl font-semibold">Processing</div>
+                        </div>
+                        <div class="lg:hidden">
+                            <div class="text-3xl font-semibold">P2P</div>
+                            <div class="text-md font-semibold">Processing</div>
                         </div>
                     </Link>
                 </div>
