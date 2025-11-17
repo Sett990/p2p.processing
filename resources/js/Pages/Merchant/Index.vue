8192f305b53c6c90aecf02dfc63abfafcbdd0abe
niiikkid <niiikkid@gmail.com>
2025-11-17 21:40:24 +0800

    Refactor Merchant Index.vue to enhance responsiveness with dual views for desktop and mobile. Implement a table layout for larger screens and a card layout for mobile, improving user experience and detail visibility across devices.

--- resources/js/Pages/Merchant/Index.vue
+++ resources/js/Pages/Merchant/Index.vue
@@ -98,61 +98,141 @@ defineOptions({ layout: AuthenticatedLayout })
                 </div>
             </template>
             <template v-slot:body>
-                <div class="overflow-x-auto card bg-base-100 shadow" v-if="viewStore.isAdminViewMode">
-                    <table class="table table-md">
-                        <thead class="text-xs uppercase bg-base-300">
-                            <tr>
-                                <th>ID</th>
-                                <th>Название</th>
-                                <th>Владелец</th>
-                                <th v-if="viewStore.isAdminViewMode">Статус</th>
-                                <th class="text-center">
-                                    <span class="sr-only">Действия</span>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="merchant in merchants.data">
-                                <th class="whitespace-nowrap">{{ merchant.id }}</th>
-                                <td>
-                                    <div class="truncate max-w-48">{{ merchant.name }}</div>
-                                    <div class="text-xs truncate max-w-36 text-base-content/70">{{ merchant.domain }}</div>
-                                </td>
-                                <td>
-                                    {{ merchant.owner.email }}
-                                </td>
-                                <td>
-                                    <div class="flex items-center text-nowrap">
-                                        <template v-if="!merchant.validated_at">
-                                            <div class="h-2.5 w-2.5 rounded-full bg-warning me-2"></div> На модерации
-                                        </template>
-                                        <template v-else-if="merchant.banned_at">
-                                            <div class="h-2.5 w-2.5 rounded-full bg-error me-2"></div> Заблокирован
-                                        </template>
-                                        <template v-else-if="merchant.active">
-                                            <div class="h-2.5 w-2.5 rounded-full bg-success me-2"></div> Включен
-                                        </template>
-                                        <template v-else>
-                                            <div class="h-2.5 w-2.5 rounded-full bg-error me-2"></div> Выключен
-                                        </template>
+                <div v-if="viewStore.isAdminViewMode" class="relative">
+                    <!-- Desktop/tablet view (table) -->
+                    <div class="hidden xl:block">
+                        <div class="overflow-x-auto card bg-base-100 shadow">
+                            <table class="table table-md">
+                                <thead class="text-xs uppercase bg-base-300">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Название</th>
+                                        <th>Владелец</th>
+                                        <th>Статус</th>
+                                        <th class="text-center">
+                                            <span class="sr-only">Действия</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="merchant in merchants.data">
+                                        <th class="whitespace-nowrap">{{ merchant.id }}</th>
+                                        <td>
+                                            <div class="truncate max-w-48">{{ merchant.name }}</div>
+                                            <div class="text-xs truncate max-w-36 text-base-content/70">{{ merchant.domain }}</div>
+                                        </td>
+                                        <td>
+                                            {{ merchant.owner.email }}
+                                        </td>
+                                        <td>
+                                            <div class="flex items-center text-nowrap">
+                                                <template v-if="!merchant.validated_at">
+                                                    <div class="h-2.5 w-2.5 rounded-full bg-warning me-2"></div> На модерации
+                                                </template>
+                                                <template v-else-if="merchant.banned_at">
+                                                    <div class="h-2.5 w-2.5 rounded-full bg-error me-2"></div> Заблокирован
+                                                </template>
+                                                <template v-else-if="merchant.active">
+                                                    <div class="h-2.5 w-2.5 rounded-full bg-success me-2"></div> Включен
+                                                </template>
+                                                <template v-else>
+                                                    <div class="h-2.5 w-2.5 rounded-full bg-error me-2"></div> Выключен
+                                                </template>
+                                            </div>
+                                        </td>
+                                        <td class="text-right">
+                                            <TableActionsDropdown>
+                                                <TableAction @click="openStatistics(merchant)">
+                                                    Статистика
+                                                </TableAction>
+                                                <TableAction @click="openPayments(merchant)">
+                                                    Платежи
+                                                </TableAction>
+                                                <TableAction @click="openSettings(merchant)">
+                                                    Настройки
+                                                </TableAction>
+                                            </TableActionsDropdown>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <!-- Mobile view (cards list) -->
+                    <div class="xl:hidden space-y-3">
+                        <div class="space-y-2">
+                            <div
+                                v-for="merchant in merchants.data"
+                                :key="merchant.id"
+                                class="card bg-base-100 shadow-sm"
+                            >
+                                <div class="card-body p-4 pt-2 pb-3">
+                                    <!-- Компактная шапка: ID и статус -->
+                                    <div class="flex justify-between items-center border-b border-neutral/50 mb-1 pb-2">
+                                        <div class="inline-flex items-center gap-2">
+                                            <span class="text-base-content/70">ID:</span>
+                                            <span class="text-base-content font-medium">{{ merchant.id }}</span>
+                                        </div>
+                                        <div class="flex items-center text-nowrap">
+                                            <template v-if="!merchant.validated_at">
+                                                <div class="h-2.5 w-2.5 rounded-full bg-warning me-2"></div>
+                                                <span class="text-xs">На модерации</span>
+                                            </template>
+                                            <template v-else-if="merchant.banned_at">
+                                                <div class="h-2.5 w-2.5 rounded-full bg-error me-2"></div>
+                                                <span class="text-xs">Заблокирован</span>
+                                            </template>
+                                            <template v-else-if="merchant.active">
+                                                <div class="h-2.5 w-2.5 rounded-full bg-success me-2"></div>
+                                                <span class="text-xs">Включен</span>
+                                            </template>
+                                            <template v-else>
+                                                <div class="h-2.5 w-2.5 rounded-full bg-error me-2"></div>
+                                                <span class="text-xs">Выключен</span>
+                                            </template>
+                                        </div>
                                     </div>
-                                </td>
-                                <td class="text-right">
-                                    <TableActionsDropdown>
-                                        <TableAction @click="openStatistics(merchant)">
-                                            Статистика
-                                        </TableAction>
-                                        <TableAction @click="openPayments(merchant)">
-                                            Платежи
-                                        </TableAction>
-                                        <TableAction @click="openSettings(merchant)">
-                                            Настройки
-                                        </TableAction>
-                                    </TableActionsDropdown>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+
+                                    <!-- Основная информация -->
+                                    <div class="flex justify-between items-center">
+                                        <div>
+                                            <div class="truncate max-w-48 min-w-48">{{ merchant.name }}</div>
+                                            <div class="text-xs truncate max-w-36 text-base-content/70">{{ merchant.domain }}</div>
+                                        </div>
+                                        <div class="hidden sm:block">
+                                            <div class="flex items-center gap-2">
+                                                <svg class="w-4 h-4 text-primary shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                                </svg>
+                                                <span class="text-base-content truncate">{{ merchant.owner.email }}</span>
+                                            </div>
+                                        </div>
+                                        <TableActionsDropdown>
+                                            <TableAction @click="openStatistics(merchant)">
+                                                Статистика
+                                            </TableAction>
+                                            <TableAction @click="openPayments(merchant)">
+                                                Платежи
+                                            </TableAction>
+                                            <TableAction @click="openSettings(merchant)">
+                                                Настройки
+                                            </TableAction>
+                                        </TableActionsDropdown>
+                                    </div>
+                                    <div class="block sm:hidden border-b border-neutral/50 my-1 w-full"></div>
+                                    <div class="sm:hidden">
+                                        <div class="flex items-center gap-2">
+                                            <svg class="w-4 h-4 text-primary shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                            </svg>
+                                            <span class="text-base-content truncate">{{ merchant.owner.email }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <section v-if="viewStore.isMerchantViewMode" class="antialiased">
