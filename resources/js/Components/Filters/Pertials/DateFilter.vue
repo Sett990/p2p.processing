10030dcd7156c6f0bd999cf0a58993d6d569cd86
niiikkid <niiikkid@gmail.com>
2025-11-26 12:36:52 +1000

    Update z-index values in DateFilter and TimepickerInput components for improved layering and visibility of icons.

--- resources/js/Components/Filters/Pertials/DateFilter.vue
+++ resources/js/Components/Filters/Pertials/DateFilter.vue
@@ -109,7 +109,7 @@ watch(model, (newValue) => {
 <template>
     <div class="form-control w-full">
         <div class="relative w-full">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none z-10">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none z-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-base-content/60">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
             </svg>
--- resources/js/Components/Form/TimepickerInput.vue
+++ resources/js/Components/Form/TimepickerInput.vue
@@ -90,7 +90,7 @@ watch(() => props.modelValue, (nv) => {
 
 <template>
     <div class="relative w-full">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none z-10">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none z-2">
             <svg class="w-4 h-4 text-base-content opacity-70" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                 <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
             </svg>
